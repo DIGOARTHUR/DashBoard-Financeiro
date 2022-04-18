0125515739e10f9e6583debd97ceb29e6b6a2d32
Diego Arthur <digo.arthur@hotmail.com>
2022-04-17 22:24:14 -0300

    Deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL:'http://digoarthur.github.io/DashBoard-Financeiro/api',
+    baseURL:'http://digoarthur.github.io/api',
 })
